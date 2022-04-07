eb211fcc3bc093e7fa54d3ce9a99d737c3ec65dd
programmingBae <abedwehh@gmail.com>
2022-04-07 20:54:09 +0700

    add image feature

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ require("dotenv").config()
 const { Client, Intents } = require('discord.js');
 const  token  = process.env.BOT_TOKEN;
 
+
 // Create a new client instance
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, 
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS]
 });
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+
+
 
 
 
 client.on("message", msg => {
     if (msg.content === "ping") {
-        msg.reply("pong!");
+        msg.channel.send({ files: ['./assets/tes.jpg'] })
+    } else if (msg.content === "padlan") {
+        msg.channel.send({ files: ['./assets/padlan.png'] })
     }
 });
 
 // Login to Discord with your client's token
-console.log(token)
+
+
 client.login(token);
